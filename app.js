6568b6301043fa7c664c7296f19214614cf3193d
mightymander <jacobdcorbett86@gmail.com>
2024-03-17 22:58:14 +0000

    installed morgan for logging
    installed ejs for displayig html files (allows dynamic stuff for later)
    
    set up views with ejs
    
    allowed the app to access files from public folder

--- app.js
+++ app.js
@@ -1,6 +1,22 @@
 const express = require('express');
+const morgan = require('morgan');
 
 //express app
 const app = express();
 
 app.listen(3000)
+
+// register view engine
+app.set('view engine', 'ejs');
+app.set('views', 'views')
+
+
+//added public static files
+app.use(express.static('public'))
+
+// 3rd party module for logging
+app.use(morgan('dev'));
+
+app.get('/', (req, res) => {
+    res.render('index')
+})
